bc4539456baacb2ab0603fedc805fb0662ecfaeb
Zalmoxes <zachault@hotmail.com>
2025-01-17 20:37:49 -0500

    More Audio
    Added my own sound effects not included in the tutorial

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+@onready var audio_stream_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died!")
+	audio_stream_player.play()
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
